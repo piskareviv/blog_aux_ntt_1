d0ebae37275d0539a55f9fa767fd7a08a4381d7b
piskarev.iv <piskarev.iv@gmail.com>
2025-04-04 04:40:28 +0300

    meow

--- run_all.sh
+++ run_all.sh
@@ -2,11 +2,12 @@ run () {
     cd $1 && echo $1 
     
     echo testing...
-    if !( g++ test.cpp -o test -std=c++20 -O2 && ./test 2> test.log && rm ./test ); then
+    if !( g++ test.cpp -o test -std=c++20 -O2 && ./test 2> test.log ); then
         echo !!!!  tests failed  !!!!
     else
         echo tests passed
     fi
+    rm ./test
 
     python3 bench.py 3 26
     python3 plot.py
