d6685552e0272aecac369fe03ba23cabcc822521
piskarev.iv <piskarev.iv@gmail.com>
2025-04-19 22:10:33 +0300

    fix cf plots

--- md_to_cf.py
+++ md_to_cf.py
@@ -9,8 +9,11 @@ for i in range(10):
     s = re.sub(r"<details[\s\S]*?>([\s\S]*?)<summary>([\s\S]*?)</summary>([\s\S]*?)</details>", r'<spoiler summary="\g<2>"> \g<1> \g<3> </spoiler>', s)
 
 s = re.sub(r"<[/]{0,1}blockquote>", r'', s)
-s = re.sub(r'src="([\s\S]*?)"', r'src="https://raw.githubusercontent.com/piskareviv/blog_aux_ntt_1/refs/heads/master/\g<1>"', s)
+s = re.sub(r'src="([\s\S]*?)"', r'src="https://raw.githubusercontent.com/piskareviv/blog_aux_ntt_1/fab3bb750120ddc7b5d6559b5b2c8972907f2087/\g<1>"', s)
 s = re.sub(r"\[<span[\s\S]*?</span>\]\(https://codeforces.com/profile/([\s\S]*?)\)", r'[user:\g<1>]', s)
+
+
+s = re.sub(r"with avx2\n", r"with avx2\n\n" + "> Text is also available on [github pages](https://piskareviv.github.io/blog_aux_ntt_1/)\n", s)
 
 
 open("main.cf", 'w').write(s)
